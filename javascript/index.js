77533d6275cbbd1b0ece40522e319097e3783ac7
your_name_here <arbazshaikh221297@gmail.com>
2023-08-06 23:22:57 +0530

    practice

--- javascript/index.js
+++ javascript/index.js
@@ -1,27 +1,15 @@
 // closures
 
+function init(){
+    var str ="Arbaz";
+    function displayName(){
+        console.log(str)
+    }
+    displayName()
+}
+init()
 
 
-
-// HOF
-
-// map
-let arr=[1,2,3,4,5,6];
-const double=arr.map((ele)=>{
-    return ele*2
-})
-console.log(double)
-
-
-
-// filter
-let arr3=[1,2,3,4,5,6];
-const filtarr=arr.filter((ele)=>{
-    return ele<3
-})
-console.log(filtarr)
-
-// foreach
 
 let arr4=[1,2,3,4,5,6];
 arr4.forEach((ele)=>{
