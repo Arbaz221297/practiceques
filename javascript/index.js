02eb47cc5b052d408af7dc4d8f608bb2ad0061a3
your_name_here <arbazshaikh221297@gmail.com>
2023-08-07 21:23:52 +0530

    practice

---
+++ javascript/index.js
@@ -0,0 +1,34 @@
+// closures
+
+function init(){
+    var str ="Arbaz";
+    function displayName(){
+        console.log(str)
+    }
+    displayName()
+}
+init()
+
+
+// HOF
+
+// map
+let arr=[1,2,3,4,5,6];
+const double=arr.map((ele)=>{
+    return ele*2
+})
+console.log(double)
+
+// reduce
+let arr2=[1,2,3,4,5,6];
+const sum=arr.reduce((acc,curr)=>{
+    return acc+curr
+})
+console.log(sum)
+
+// filter
+let arr3=[1,2,3,4,5,6];
+const filtarr=arr.filter((ele)=>{
+    return ele<3
+})
+console.log(filtarr)
