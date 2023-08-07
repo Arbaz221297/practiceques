507c21ae8a7b23251b0bbdded15da26402b88eb9
your_name_here <arbazshaikh221297@gmail.com>
2023-08-07 23:19:54 +0530

    practice

--- javascript/index.js
+++ javascript/index.js
@@ -19,15 +19,14 @@ const double=arr.map((ele)=>{
 })
 console.log(double)
 
-// reduce
-let arr2=[1,2,3,4,5,6];
-const sum=arr.reduce((acc,curr)=>{
-    return acc+curr
-})
-console.log(sum)
+
 
 // filter
-
+let arr3=[1,2,3,4,5,6];
+const filtarr=arr.filter((ele)=>{
+    return ele<3
+})
+console.log(filtarr)
 
 // foreach
 
